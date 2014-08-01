b5a499c963f645ce0eb8bf12a027b7be412e8cda
Scott Kingsley Clark <lol@scottkclark.com>
2014-08-01 06:18:53 -0500

    Add editor field type

---
+++ fields/class-editor-field.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class WP_Editor_Field
+ */
+class WP_Editor_Field extends WP_Field_Base {
+
+	/**
+	 *
+	 */
+	const HTML_TAG = 'textarea';
+
+	/**
+	 *
+	 */
+	const FIELD_TYPE = 'textarea';
+
+
+
+}
