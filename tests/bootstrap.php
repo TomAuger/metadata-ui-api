f245935e28f2ec5dba66ed597f0b22edad447a40
clubduece <daryl@clubduece.com>
2014-09-06 16:45:28 +0000

    Add codebase constant

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 
+define('CODEBASE', dirname(dirname( __FILE__)));
+
 require_once getenv('WP_TESTS_DIR') . '/tests/phpunit/includes/functions.php';
 
 require getenv('WP_TESTS_DIR') . '/tests/phpunit/includes/bootstrap.php';
